6542171390a293d876f5269d960a101d23bf0840
bmjoy <ironmandev@gmail.com>
2020-07-21 15:10:45 +0800

    unity2019.4.2     C# singleton     MonoBehaviour singleton     MonoBehaviour singleton with DontDestroyOnLoad

---
+++ Assets/Scripts/Common/Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿class Singleton<T> where T : class, new()
+{
+    private static T _instance;
+    private static readonly object syslock = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            if (null == _instance)
+            {
+                lock (syslock)
+                {
+                    if (null == _instance)
+                    {
+                        _instance = new T();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
